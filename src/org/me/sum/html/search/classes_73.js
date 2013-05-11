7cc8e6d7bb6029f97c65053d51b19d56decebbec
Ivan <8observer8@gmail.com>
2013-05-11 23:46:27 +0400

    The Sum from 1 to n.

--- src/org/me/sum/html/search/classes_73.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['sum',['Sum',['../classorg_1_1me_1_1sum_1_1_sum.html',1,'org::me::sum']]]
-];
--- src/org/me/sum/html/search/functions_73.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['sum',['sum',['../classorg_1_1me_1_1sum_1_1_sum.html#a8104eab790cbf3a336dc14171d4b96c7',1,'org::me::sum::Sum']]]
-];
